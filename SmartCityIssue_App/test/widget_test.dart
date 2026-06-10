40c806d48d19f4e067550652bf806b66be322623
Manjunathvpoojari <mm4211051@gmail.com>
2026-06-10 11:26:03 +0530

    SS added

--- SmartCityIssue_App/test/widget_test.dart
+++ SmartCityIssue_App/test/widget_test.dart
@@ -7,8 +7,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shimmer/main.dart';
 
-import 'package:smart_city/main.dart';
+// ignore: unused_import
+import 'package:smart_city_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
